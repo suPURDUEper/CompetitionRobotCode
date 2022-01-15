3453117ebc58827c765176deb0e85f2489ae279b
Percsl4y3r <noah.smith@pphs.purdue.edu>
2022-01-15 15:22:19 -0500

    finished intake subsystem

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,12 +4,18 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
-  CanSpark
-  public Intake() {}
+  CANSparkMax intakeMotor;
+  public Intake() {
+    intakeMotor = new CANSparkMax(Constants.IntakeMotor, MotorType.kBrushless);
+  }
 
   @Override
   public void periodic() {
