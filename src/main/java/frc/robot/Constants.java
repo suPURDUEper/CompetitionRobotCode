d62a533c4d65fc9eaa4e1ea164a1994668cd174f
Percsl4y3r <noah.smith@pphs.purdue.edu>
2022-01-15 15:12:48 -0500

    added indexer subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,33 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    //PWM's for wiring
+    public static final int LeftFront = 1;
+    public static final int LeftBack = 2;
+    public static final int RightFront = 3;
+    public static final int RightBack = 4;
+    public static final int ClimbMotor = 3;
+    public static final int IntakeMotor = 0;
+    public static final int IndexerMotor1 = 5;
+    public static final int IndexerMotor2 = 6;
+    public static final int IndexerMotor3 = 7;
+
+    //Controller axis
+    public static final int XboxLeftYAxis = 1;
+    public static final int XboxRightXAxis = 4;
+    public static final int XboxRightYAxis = 5;
+    //autonomous constants
+    public static final double DriveForwardTime = 3;
+    public static final double AutonomousSpeed = 0.4;
+    //joystick
+    public static final int DriveJoystickNumber = 0;
+    public static final int OperatorJoystickNumber = 1;
+    //drivetrain variables
+    public static final double BoostActive = 0.9;
+    public static final double BoostInactive = 0.7;
+    public static final double DriveTrainCurve = 0.15;
+    public static final double DeadZone = 0.05;
+    public static final double BaseVelocity = 0.14;
+    public static final double DriveTrainSpeed = 0.7;
+}
