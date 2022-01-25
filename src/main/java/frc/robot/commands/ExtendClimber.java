8a9261f97f4ad75be6d0bd377e294c8e0cf5be2b
Zachary Kaster <zckaster@iu.edu>
2022-01-25 17:14:45 -0500

    reworked the climber system to include the pneumatics, also added the intake pneumatics

--- src/main/java/frc/robot/commands/ExtendClimber.java
+++ src/main/java/frc/robot/commands/ExtendClimber.java
@@ -5,16 +5,16 @@
 package frc.robot.commands;
 
 import frc.robot.RobotContainer;
-import frc.robot.subsystems.Pneumatics;
+import frc.robot.subsystems.Climber;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class ExtendClimber extends CommandBase {
-  public final Pneumatics pneumatics;
+  public final Climber mClimber;
   /** Creates a new ExtendClimber. */
-  public ExtendClimber(Pneumatics mPneumatics) {
+  public ExtendClimber(Climber climber) {
     // Use addRequirements() here to declare subsystem dependencies.
-    pneumatics = mPneumatics;
-    addRequirements(mPneumatics);
+    mClimber = climber;
+    addRequirements(climber);
   }
 
   // Called when the command is initially scheduled.
@@ -24,7 +24,7 @@ public class ExtendClimber extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    pneumatics.controlSolenoid(RobotContainer.operatorJoyStick);
+    mClimber.controlSolenoid(RobotContainer.operatorJoyStick);
   }
 
   // Called once the command ends or is interrupted.
